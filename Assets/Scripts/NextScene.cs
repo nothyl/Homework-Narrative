60c1362cfba58730a32ebb62dccc8f94ebdf935e
nothyl <hanyuli488@gmail.com>
2025-02-22 11:46:16 -0500

    Realm Chatter

---
+++ Assets/Scripts/NextScene.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class NextScene : MonoBehaviour
+{
+    public string nextLevel = "Scene_2";
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    { }
+        private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Finish":
+                {
+                    SceneManager.LoadScene(nextLevel);
+                    break;
+
+                }
+        }
+    }
+}
